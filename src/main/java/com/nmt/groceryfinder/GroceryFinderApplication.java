b2bcefe83f3623c47afec0dc56f7ead365ac3492
Nguyen Minh Thuan <96782417+nmthuann@users.noreply.github.com>
2024-09-20 12:53:02 +0700

    commit

---
+++ src/main/java/com/nmt/groceryfinder/GroceryFinderApplication.java
@@ -0,0 +1,13 @@
+package com.nmt.groceryfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nmt/groceryfinder/GroceryFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nmt.groceryfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
