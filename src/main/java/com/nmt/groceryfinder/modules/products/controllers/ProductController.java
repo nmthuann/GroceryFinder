91c0108f41dbea24c5e36ba10dfe66af28b88650
Nguyen Minh Thuan <96782417+nmthuann@users.noreply.github.com>
2024-10-09 21:45:06 +0700

    fix: add api in Product controller.

--- src/main/java/com/nmt/groceryfinder/modules/products/controllers/ProductController.java
+++ src/main/java/com/nmt/groceryfinder/modules/products/controllers/ProductController.java
@@ -26,7 +26,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
